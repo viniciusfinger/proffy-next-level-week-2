2c1b0c77a13b4d2139c13c85e1ae2e82c2773566
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-05 18:54:11 -0300

    Aula 2 - Alteracoes no server

---
+++ server/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up (knex: Knex) {
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary();
+
+        
+        table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+
+        table.timestamp('created_at').defaultTo('now()')
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTableIfExists('class_schedule');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get('/users' , (request, response) => {
-    return response.json({message:"Hello World"})
-
-});
-
+app.use(routes);
 
 app.listen(3333);
