2ba5276c8be750b89a11f3985ef605717b2318ca
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-05 18:09:37 -0300

    Dia 2

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/users' , (request, response) => {
+    return response.json({message:"Hello World"})
+
+});
+
+
+app.listen(3333);
