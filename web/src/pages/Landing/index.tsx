053db1f3f518eca85e03be7720a0a5796d2f82dd
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-09 13:46:46 -0300

    Dia 4

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
@@ -9,8 +9,20 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import api from '../../services/api';
 
-const Landing: React.FC = () => {
+ function Landing() { {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response =>{
+      const {total} = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+ 
+
   return (
     <div id="page-landing">
       <div className="container" id="page-landing-content">
@@ -37,12 +49,13 @@ const Landing: React.FC = () => {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões realizadas.{" "}
+  Total de {totalConnections} conexões realizadas.{" "}
           <img src={purpleHeartIcon} alt="Coração Roxo" />
         </span>
       </div>
     </div>
-  );
+  ); 
+}
 }
 
 export default Landing;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,26 +4,43 @@ import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select'
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+        <Select
+              name="subject"
+              label="Matéria"
+              options={[
+                { value: "Artes", label: "Artes" },
+                { value: "Biologia", label: "Biologia" },
+                { value: "Geografia", label: "Geografia" },
+                { value: "Matemática", label: "Matemática" },
+                { value: "Inglês", label: "Inglês" },
+                { value: "História", label: "História" },
+                { value: "Física", label: "Física" },
+                { value: "Português", label: "Português" },
+                { value: "Química", label: "Química" },
+              ]}
+            />
+          <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-feira" },
+                { value: "2", label: "Terca-feira" },
+                { value: "3", label: "Quarta-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sabado" },
+              ]}
+            />
+          <Input name="time" type="time" label="Hora" />
         </form>
       </PageHeader>
 
